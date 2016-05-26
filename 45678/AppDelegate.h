8f66a8b1f74ad3040656cf78bbbadcb636840933
cjwddyr <497528326@qq.com>
2016-05-26 11:27:56 +0800

    Initial Commit

---
+++ 45678/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  45678
+//
+//  Created by mac on 16/5/26.
+//  Copyright (c) 2016年 emo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 45678/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  45678
+//
+//  Created by mac on 16/5/26.
+//  Copyright (c) 2016年 emo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
